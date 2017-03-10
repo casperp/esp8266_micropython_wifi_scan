778a93811d23890c2a0204b4cf5572a5553d043a
casper <casperprins98@gmail.com>
2017-03-10 22:30:45 +0100

    Update main.py

--- multiple files/main.py
+++ multiple files/main.py
@@ -2,12 +2,21 @@ import webrepl
 import network
 import displaywifi
 webrepl.start()
-"default pins 5, 4 "
+"""" import network,displaywifi module and webrepl for http://micropython.org/webrepl/?
+default connection:
+esp8266 -> OLED display
+5(d1)   ->  scl
+6(d2)   ->  sda
+GND     ->  GND
+3.3v    ->  vcc"""
+
 
 
 def main():
     display = displaywifi.DisplayWifi(5,4)
+    # use the displaywifi on the pins 4,5
     while True:
         display.format()
         print(display)
+        # print the information thatś shown on the screen.
 main()
